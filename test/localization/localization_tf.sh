b075ce3375071a672c5c3b1f0e01e2fdda60979d
Volt <volt.autonomous@gmail.com>
2022-06-14 12:32:37 +0300

    localization related changes

--- test/localization/localization_tf.sh
+++ test/localization/localization_tf.sh
@@ -3,5 +3,5 @@
 source /opt/ros/galactic/setup.bash && 
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp &&
 source /home/volt/projects/gnss_ins_localizer_ws/install/setup.bash && 
-ros2 launch gnss_ins_localization_nodes tf_lidars.launch.py
+ros2 launch gnss_ins_localization_nodes gnss_ins_localization_launch.py
 
