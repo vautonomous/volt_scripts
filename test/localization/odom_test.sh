7fd37c24c02ee8cbf30fd5c4842bfb0251930b4c
Volt <volt.autonomous@gmail.com>
2022-06-17 16:04:46 +0300

    add odom_test

--- test/localization/odom_test.sh
+++ test/localization/odom_test.sh
@@ -3,4 +3,4 @@
 source /opt/ros/galactic/setup.bash && 
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp &&
 source /home/volt/projects/gnss_ins_localizer_ws/install/setup.bash && 
-ros2 launch gnss_ins_msg_buffer gnss_ins_msg_buffer_launch.py 
+ros2 run odom_test odom_test
--- test/localization/test_localization.sh
+++ test/localization/test_localization.sh
@@ -5,7 +5,7 @@ dir_main="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 array_path_scripts=()
 #array_path_scripts+=("${dir_main}/localization_autoware.sh")
 array_path_scripts+=("${dir_main}/localization_tf.sh")
-#array_path_scripts+=("${dir_main}/msg_buffer_localization.sh")
+array_path_scripts+=("${dir_main}/odom_test.sh")
 #array_path_scripts+=("${dir_main}/lidar_launch.sh")
 array_path_scripts+=("${dir_main}/applanix_launch.sh")
 
