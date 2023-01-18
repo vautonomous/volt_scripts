321e613caa65e4f0a84eca4c9838451c6c17125e
volt autonomous <volt.autonomous@gmail.com>
2023-01-18 18:08:03 +0300

    use autoware rviz

--- launch_scripts/run_rviz.sh
+++ launch_scripts/run_rviz.sh
@@ -2,4 +2,4 @@
 
 source /opt/ros/humble/setup.bash
 source ~/projects/autoware/install/setup.bash
-ros2 run rviz2 rviz2 -d ~/projects/volt_scripts/config/autoware.rviz --name rviz_volt -s "IHateTheWorld"
+ros2 run rviz2 rviz2 -d ~/projects/autoware/src/launcher/autoware_launch/autoware_launch/rviz/autoware.rviz --name rviz_volt
