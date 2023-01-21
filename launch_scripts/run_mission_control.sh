84540d713f8c4912368a7329792f7301a56fa962
volt autonomous <volt.autonomous@gmail.com>
2023-01-21 11:13:19 +0300

    revert back to turkish

--- launch_scripts/run_mission_control.sh
+++ launch_scripts/run_mission_control.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+#/home/volt/programs/volt_gui/Mission_Control_MultiLang.AppImage
 /home/volt/programs/volt_gui/Mission_Control_Turkish.AppImage
--- launch_scripts/run_mission_observation.sh
+++ launch_scripts/run_mission_observation.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+#/home/volt/programs/volt_gui/Mission_Observation_MultiLang.AppImage
 /home/volt/programs/volt_gui/Mission_Observation_Turkish.AppImage
