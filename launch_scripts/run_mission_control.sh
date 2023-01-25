ad01303211f514bc5b7d7dcf8cd22d5da99b3e2c
volt autonomous <volt.autonomous@gmail.com>
2023-01-25 15:15:40 +0300

    new guis

--- launch_scripts/run_mission_control.sh
+++ launch_scripts/run_mission_control.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-#/home/volt/programs/volt_gui/Mission_Control_MultiLang.AppImage
-/home/volt/programs/volt_gui/Mission_Control_Turkish.AppImage
+#/home/volt/programs/volt_gui/Mission_Control_Turkish.AppImage
+/home/volt/programs/volt_gui/Mission_Control_New.AppImage
--- launch_scripts/run_mission_observation.sh
+++ launch_scripts/run_mission_observation.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-#/home/volt/programs/volt_gui/Mission_Observation_MultiLang.AppImage
-/home/volt/programs/volt_gui/Mission_Observation_Turkish.AppImage
+#/home/volt/programs/volt_gui/Mission_Observation_Turkish.AppImage
+/home/volt/programs/volt_gui/Mission_Observation_New.AppImage
